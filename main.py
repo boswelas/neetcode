d0f128e0a25249392f1ac38469e2375465006043
Ashley Boswell <boswelas@oregonstate.edu>
2023-12-11 13:31:32 -0600

    solved twoSum()

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # 'NeetCode Roadmap'
-
 # 'Dec 11, 2023'
 
 class Solution:
@@ -16,7 +15,6 @@ class Solution:
 
         return False
 
-
     def isAnagram(self, s: str, t: str) -> bool:
         """Given two strings s and t, return true if t is an anagram of s, and false otherwise.
         An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using
@@ -27,9 +25,25 @@ class Solution:
 
         return sorted_s == sorted_t
 
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        """Given an array of integers nums and an integer target, return indices of the two numbers such that they add
+        up to target.
+        You may assume that each input would have exactly one solution, and you may not use the same element twice.
+        You can return the answer in any order."""
+
+        num_dict = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_dict:
+                return [num_dict[complement], i]
+            num_dict[num] = i
+
+        return []
+
 
 solution = Solution()
-s = "rat"
-t = "car"
-result = solution.isAnagram(s, t)
+nums = [3, 2, 4]
+target = 6
+result = solution.twoSum(nums, target)
 print(result)
