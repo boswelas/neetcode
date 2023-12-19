685d156ab5d8e19c852e53bd2a54146d84c45072
Ashley Boswell <boswelas@oregonstate.edu>
2023-12-19 13:55:43 -0600

    solved threeSum()

--- main.py
+++ main.py
@@ -238,9 +238,40 @@ class Solution:
 
         return []
 
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        """Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k,
+        and j != k, and nums[i] + nums[j] + nums[k] == 0.
+        Notice that the solution set must not contain duplicate triplets."""
+        nums.sort()
+        result = []
+
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            left, right = i + 1, len(nums) - 1
+
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+
+                if total == 0:
+                    result.append([nums[i], nums[left], nums[right]])
+
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+
+                    left += 1
+                    right -= 1
+                elif total < 0:
+                    left += 1
+                else:
+                    right -= 1
+
+        return result
 
 solution = Solution()
-numbers = [5,25,75]
-target = 100
-result = solution.twoSum_2(numbers, target)
+nums = [-2,0,0,2,2]
+result = solution.threeSum(nums)
 print(result)
