fa643a7fdaa92c14dad915000b8fc6c2f27dba02
Ashley Boswell <boswelas@oregonstate.edu>
2024-01-11 16:27:37 -0600

    solved isValid()

--- main.py
+++ main.py
@@ -348,8 +348,42 @@ class Solution:
 
         return answer
 
+    def isValid(self, s: str) -> bool:
+        """Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input
+        string is valid.
+        An input string is valid if:
+        Open brackets must be closed by the same type of brackets.
+        Open brackets must be closed in the correct order.
+        Every close bracket has a corresponding open bracket of the same type."""
+        stack = []
+
+        for char in s:
+            if char in "({[":
+                stack.append(char)
+            elif char == "}":
+                if len(stack) > 0 and stack[-1] == "{":
+                    stack.pop()
+                else:
+                    stack.append(char)
+            elif char == ")":
+                if len(stack) > 0 and stack[-1] == "(":
+                    stack.pop()
+                else:
+                    stack.append(char)
+            elif char == "]":
+                if len(stack) > 0 and stack[-1] == "[":
+                    stack.pop()
+                else:
+                    stack.append(char)
+
+        if len(stack) == 0:
+            return True
+
+        return False
+
+
 
 solution = Solution()
-height = [2,0,2]
-result = solution.trap(height)
+s = "]"
+result = solution.isValid(s)
 print(result)
