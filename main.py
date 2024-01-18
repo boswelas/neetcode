d38527bf9a1ed144a08b81df4139e599b95c9502
Ashley Boswell <boswelas@oregonstate.edu>
2024-01-18 11:23:31 -0600

    solve search()

--- main.py
+++ main.py
@@ -533,9 +533,28 @@ class Solution:
 
         return max_area
 
+    def search(self, nums: list[int], target: int) -> int:
+        """Given an array of integers nums which is sorted in ascending order, and an integer target, write a function
+        to search target in nums. If target exists, then return its index. Otherwise, return -1.
+        You must write an algorithm with O(log n) runtime complexity."""
+        low = 0
+        high = len(nums) - 1
+
+        while low <= high:
+            mid = (high + low) // 2
+            if nums[mid] < target:
+                low = mid + 1
+            elif nums[mid] > target:
+                high = mid - 1
+            else:
+                return mid
+
+        return -1
+
 
 solution = Solution()
-heights = [2, 4]
-result = solution.largestRectangleArea(heights)
+nums = [-1,0,3,5,9,12]
+target = 5
+result = solution.search(nums, target)
 print(result)
 print("expected: 4")
