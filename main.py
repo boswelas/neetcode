bfd27ff96bc189bed0bbc740eead1a0b38dc0fb3
Ashley Boswell <boswelas@oregonstate.edu>
2024-01-19 11:16:02 -0600

    solution for findMin

--- main.py
+++ main.py
@@ -616,13 +616,31 @@ class Solution:
 
         return low
 
+    def findMin(self, nums: List[int]) -> int:
+        """Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example,
+        the array nums = [0,1,2,4,5,6,7] might become:
+        [4,5,6,7,0,1,2] if it was rotated 4 times.
+        [0,1,2,4,5,6,7] if it was rotated 7 times.
+        Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1],
+        a[2], ..., a[n-2]].
+        Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+        You must write an algorithm that runs in O(log n) time."""
+        left = 0
+        right = len(nums) - 1
 
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right = mid
+
+        return nums[left]
 
 
 
 solution = Solution()
-piles = [3,6,7,11]
-h = 8
-result = solution.minEatingSpeed(piles, h)
+nums = [11,13,15,17]
+result = solution.findMin(nums)
 print(result)
-print("expected: 4")
+print("expected: 1")
