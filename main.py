6ae553e6c5022ae8b3fdbd748a2d57613966a56b
Ashley Boswell <boswelas@oregonstate.edu>
2024-01-22 10:14:43 -0600

    solution for findMedianSortedArrays()

--- main.py
+++ main.py
@@ -702,11 +702,43 @@ class Solution:
 
             return res
 
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        """Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted
+        arrays.
+        The overall run time complexity should be O(log (m+n))."""
+        A = nums1
+        B = nums2
+        total = len(nums1) + len(nums2)
+        half = total // 2
+
+        if len(B) < len(A):
+            A, B = B, A
+
+        left = 0
+        right = len(A) - 1
+
+        while True:
+            i = (left + right) // 2
+            j = half - i - 2
+            Aleft = A[i] if i >= 0 else float("-infinity")
+            Aright = A[i + 1] if (i + 1) < len(A) else float("infinity")
+            Bleft = B[j] if j >= 0 else float("-infinity")
+            Bright = B[j + 1] if (j + 1) < len(B) else float("infinity")
+
+            if Aleft <= Bright and Bleft <= Aright:
+                if total % 2:
+                    return min(Aright, Bright)
+                return (max(Aleft, Bleft) + (min(Aright, Bright))) / 2
+            elif Aleft > Bright:
+                right = i - 1
+            else:
+                left = i + 1
+
 
 
 solution = Solution()
-nums = [4, 5, 6, 7, 0, 1, 2]
-target = 6
-result = solution.search2(nums, target)
+nums1 = [1,3]
+nums2 = [2]
+result = solution.findMedianSortedArrays(nums1, nums2)
 print(result)
 print("expected: 4")
