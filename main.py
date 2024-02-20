acb621eac7fa994d19d6ed1969467deeefbd9202
Ashley Boswell <boswelas@oregonstate.edu>
2024-02-20 14:12:48 -0800

    solution for minWindow()

--- main.py
+++ main.py
@@ -819,9 +819,45 @@ class Solution:
                 return True
         return False
 
+    def minWindow(self, s: str, t: str) -> str:
+        """Given two strings s and t of lengths m and n respectively, return the minimum window substring of s such that
+         every character in t (including duplicates) is included in the window. If there is no such substring, return
+         the empty string "". The testcases will be generated such that the answer is unique."""
+        if t == "":
+            return ""
+
+        countT = {}
+        window = {}
+
+        for c in t:
+            countT[c] = 1 + countT.get(c, 0)
+
+        have, need = 0, len(countT)
+        res, resLen = [-1, -1], float("infinity")
+        l = 0
+
+        for r in range(len(s)):
+            c = s[r]
+            window[c] = 1 + window.get(c, 0)
+
+            if c in countT and window[c] == countT[c]:
+                have += 1
+
+            while have == need:
+                if (r - l + 1) < resLen:
+                    res = [l, r]
+                    resLen = (r - l + 1)
+                window[s[l]] -= 1
+                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                    have -= 1
+                l += 1
+
+        l, r = res
+        return s[l:r+1] if resLen != float("infinity") else ""
+
 solution = Solution()
-s1 = "ab"
-s2 = "eidbaooo"
-result = solution.checkInclusion(s1, s2)
+s = "ADOBECODEBANC"
+t = "ABC"
+result = solution.minWindow(s, t)
 print(result)
 print("expected: true")
