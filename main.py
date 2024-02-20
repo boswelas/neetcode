9dddc23d62ec5629fe84fdade5fd61b47bf9d482
Ashley Boswell <boswelas@oregonstate.edu>
2024-02-20 10:34:28 -0800

    solution for maxProfit()

--- main.py
+++ main.py
@@ -734,11 +734,28 @@ class Solution:
             else:
                 left = i + 1
 
+    def maxProfit(self, prices: List[int]) -> int:
+        """You are given an array prices where prices[i] is the price of a given stock on the ith day.
+        You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the
+        future to sell that stock.
+        Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0."""
+        left = 0
+        right = 1
+        maxProfit = 0
+
+        while right < len(prices):
+            if prices[left] < prices[right]:
+                profit = prices[right] - prices[left]
+                maxProfit = max(maxProfit, profit)
+            else:
+                left = right
+            right += 1
+
+        return maxProfit
 
 
 solution = Solution()
-nums1 = [1,3]
-nums2 = [2]
-result = solution.findMedianSortedArrays(nums1, nums2)
+prices = [7,1,5,3,6,4]
+result = solution.maxProfit(prices)
 print(result)
 print("expected: 4")
