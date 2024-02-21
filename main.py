c84459b6ea77552595aea2814c2931b9a7261041
Ashley Boswell <boswelas@oregonstate.edu>
2024-02-21 11:08:57 -0800

    solution for reorderList()

--- main.py
+++ main.py
@@ -922,6 +922,36 @@ class Solution:
 
         return dummy.next
 
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """You are given the head of a singly linked-list. The list can be represented as: L0 → L1 → … → Ln - 1 → Ln
+        Reorder the list to be on the following form: L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+        You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+        Do not return anything, modify head in-place instead."""
+        slow, fast = head, head.next
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        second = slow.next
+        prev = slow.next = None
+
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+
+        first = head
+        second = prev
+        while second:
+            temp1, temp2 = first.next, second.next
+            first.next = second
+            second.next = temp1
+            first = temp1
+            second = temp2
+
+        return
 
 
 solution = Solution()
