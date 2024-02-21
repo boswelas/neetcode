0ec3c9efb015e1fc1b23d87bce08ecfd81c50867
Ashley Boswell <boswelas@oregonstate.edu>
2024-02-21 11:20:22 -0800

    solution for removeNthFromEnd()

--- main.py
+++ main.py
@@ -950,9 +950,25 @@ class Solution:
             second.next = temp1
             first = temp1
             second = temp2
-
         return
 
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        """Given the head of a linked list, remove the nth node from the end of the list and return its head."""
+        dummy = ListNode(0, head)
+        left = dummy
+        right = head
+
+        while n > 0 and right:
+            right = right.next
+            n -= 1
+
+        while right:
+            left = left.next
+            right = right.next
+
+        left.next = left.next.next
+
+        return dummy.next
 
 solution = Solution()
 result = solution.reverseList()
