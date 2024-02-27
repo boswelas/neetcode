c6f4c586708ccc7b26aa68bacf03169eff733001
Ashley Boswell <boswelas@oregonstate.edu>
2024-02-26 20:37:53 -0800

    solution for addTwoNumbers()

--- main.py
+++ main.py
@@ -1005,10 +1005,36 @@ class Solution:
 
         return oldToCopy[head]
 
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        """You are given two non-empty linked lists representing two non-negative integers. The digits are stored in
+        reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a
+        linked list.
+        You may assume the two numbers do not contain any leading zero, except the number 0 itself."""
+        dummy = ListNode()
+        cur = dummy
+        carry = 0
+
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+
+            val = v1 + v2 + carry
+            carry = val // 10
+            val = val % 10
+            cur.next = ListNode(val)
+
+            cur = cur.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        return dummy.next
+
 
 
 
 solution = Solution()
-result = solution.reverseList()
+l1 = ListNode(2, ListNode(4, ListNode(3)))
+l2 = ListNode(5, ListNode(6, ListNode(4)))
+result = solution.addTwoNumbers(l1, l2)
 print(result)
 print("expected: true")
