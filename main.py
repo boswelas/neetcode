dff8b6129090070f814ba3698d51b8aa5be73afb
Ashley Boswell <boswelas@oregonstate.edu>
2024-02-26 20:19:20 -0800

    solution for copyRandomList()

--- main.py
+++ main.py
@@ -12,6 +12,13 @@ class ListNode:
         self.next = next
 
 
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+
+
 class Solution:
     def containsDuplicate(self, nums: list[int]) -> bool:
         """Given an integer array nums, return true if any value appears at least twice in the array, and return false
@@ -969,6 +976,37 @@ class Solution:
         left.next = left.next.next
 
         return dummy.next
+
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        """A linked list of length n is given such that each node contains an additional random pointer, which could
+        point to any node in the list, or null.
+        Construct a deep copy of the list. The deep copy should consist of exactly n brand new nodes, where each new
+        node has its value set to the value of its corresponding original node. Both the next and random pointer of the
+        new nodes should point to new nodes in the copied list such that the pointers in the original list and copied
+        list represent the same list state. None of the pointers in the new list should point to nodes in the original
+        list.
+        For example, if there are two nodes X and Y in the original list, where X.random --> Y, then for the
+        corresponding two nodes x and y in the copied list, x.random --> y.
+        Return the head of the copied linked list."""
+        oldToCopy = {None : None}
+
+        cur = head
+        while cur:
+            copy = Node(cur.val)
+            oldToCopy[cur] = copy
+            cur = cur.next
+
+        cur = head
+        while cur:
+            copy = oldToCopy[cur]
+            copy.next = oldToCopy[cur.next]
+            copy.random = oldToCopy[cur.random]
+            cur = cur.next
+
+        return oldToCopy[head]
+
+
+
 
 solution = Solution()
 result = solution.reverseList()
