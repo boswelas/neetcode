c057a283c69147999d57ab299afcca4e23ff6183
Ashley Boswell <boswelas@oregonstate.edu>
2024-04-29 10:47:11 -0700

    solved isValidSudoku()

--- main.py
+++ main.py
@@ -131,8 +131,43 @@ class Solution:
 
         return output
 
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        """Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the
+        following rules:
+        Each row must contain the digits 1-9 without repetition.
+        Each column must contain the digits 1-9 without repetition.
+        Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition."""
+        # create DS for r and c, square
+        # traverse board and add to r and c in dict
+        # check if in key, if not add, else return false
+        rows = defaultdict(set)
+        columns = defaultdict(set)
+        squares = defaultdict(set)
+
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == ".":
+                    continue
+                elif board[i][j] in rows[i] or board[i][j] in columns[j] or board[i][j] in squares[i//3, j//3]:
+                    return False
+                else:
+                    rows[i].add(board[i][j])
+                    columns[j].add(board[i][j])
+                    squares[i//3, j//3].add(board[i][j])
+
+        return True
+
+
 solution = Solution()
-nums = [0, 0]
-result = solution.productExceptSelf(nums)
+board = [["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+result = solution.isValidSudoku(board)
 print(result)
 
