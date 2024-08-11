ac47b8e8ac93ef9a4c41c6dcb7817fe49c96f20e
Ashley Boswell <boswelas@oregonstate.edu>
2024-08-11 16:09:54 -0700

    solved searchMatrix()

--- binary_search.py
+++ binary_search.py
@@ -20,9 +20,32 @@ class Solution:
                 l = mid + 1
                         
         return -1
+    
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        """You are given an m x n 2-D integer array matrix and an integer target.
+        Each row in matrix is sorted in non-decreasing order.
+        The first integer of every row is greater than the last integer of the previous row.
+        Return true if target exists within matrix or false otherwise.
+        Can you write a solution that runs in O(log(m * n)) time?"""
+        for i in range(len(matrix)-1, -1, -1):
+            if matrix[i][0] <= target:
+                break  
+        
+        l, r = 0, len(matrix[i]) - 1
+        
+        while l <= r:
+            mid = l + ((r - l) // 2)
+            if matrix[i][mid] == target:
+                return True
+            elif matrix[i][mid] > target:
+                r = mid - 1
+            else:
+                l = mid + 1
+        return False
+
         
         
 solution = Solution()
-nums = [-1,0,2,4,6,8]
-target = 4
-print(solution.search(nums, target))
+matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+target = 3
+print(solution.searchMatrix(matrix, target))
