524f15dbd257aada68e7277d0fc50a536e904f45
Ashley Boswell <boswelas@oregonstate.edu>
2024-09-23 15:39:31 -0700

    solution minCostClimbingStairs()

--- 1d_dynamic_programming.py
+++ 1d_dynamic_programming.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
         def climbStairs(self, n: int) -> int:
             """You are given an integer n representing the number of steps to reach the top of a staircase. 
@@ -13,10 +16,21 @@ class Solution:
                 n1 = n2
                 n2 = temp
             return n2
-                        
+        
+        def minCostClimbingStairs(self, cost: List[int]) -> int:
+            """You are given an array of integers cost where cost[i] is the cost of taking a step from the ith floor 
+            of a staircase. After paying the cost, you can step to either the (i + 1)th floor or the (i + 2)th floor.
+            You may choose to start at the index 0 or the index 1 floor.
+            Return the minimum cost to reach the top of the staircase, i.e. just past the last index in cost."""
+            cost.append(0)
+            
+            for i in range(len(cost)-3, -1, -1):
+                cost[i] += min(cost[i + 1], cost[i + 2])
+                
+            return min(cost[0], cost[1])
                 
 
 
 solution = Solution()
-n = 5
-print(solution.climbStairs(n))
+cost = [10, 15, 20]
+print(solution.minCostClimbingStairs(cost))
