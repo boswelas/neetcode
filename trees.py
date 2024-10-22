3b1441858be18d2fb2b7a14cdc990bea3eb2fca3
Ashley Boswell <boswelas@oregonstate.edu>
2024-10-22 16:36:35 -0700

    solved invertTree()

--- trees.py
+++ trees.py
@@ -11,15 +11,14 @@ class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         """You are given the root of a binary tree root. Invert the binary tree and return its root."""
         if not root:
-            return None
+            return 
         
         root.left, root.right = root.right, root.left
-        
-        self.invertTree(root.left)
+        self.invertTree(root.left)  
         self.invertTree(root.right)
-        
         return root
-        
+         
+       
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         stack = [root, 1]
         result = 0
@@ -81,6 +80,56 @@ class Solution:
             
             return dfs(p.left, q.left) and dfs(p.right, q.right)
         return dfs(p, q)
+
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        
+        def sameTree(p, q):
+            if not p and not q:
+                return True
+            
+            if (p and not q) or (q and not p):
+                return False
+            
+            
+            if p.val != q.val:
+                return False
+            
+            return sameTree(p.left, q.left) and sameTree(p.right, q.right)
+        
+        def has_subtree(root):
+            if not root:
+                return False
+            
+            if sameTree(root, subRoot):
+                return True
+            
+            return has_subtree(root.left) or has_subtree(root.right)
+        
+        return has_subtree(root)
+
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        lca = [root]
+        
+        def search(root):
+            if not root: 
+                return
+            
+            lca[0] = root
+            
+            if root is p or root is q:
+                return
+            
+            elif root.val < p.val and root.val < q.val:
+                search(root.right)
+            elif root.val > p.val and root.val > q.val:
+                search(root.left)
+            else:
+                return
+            
+        search(root)
+        return lca[0]
+            
+            
 
 solution = Solution()    
 root_values = [1,2,3,None,None,4]
